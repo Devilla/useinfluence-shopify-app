c4511518731884d787a808925d387c8801a0b3e0
devilla <dev.koold@gmail.com>
2018-09-03 09:11:08 +0530

    heroku port andd server

--- bin/www.js
+++ bin/www.js
@@ -4,14 +4,20 @@ const chalk = require('chalk');
 const http = require('http');
 const app = require('../server');
 
-const port = '3009';
-app.set('port', port);
+// const port = '3000';
+// app.set('port', port);
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port, err => {
-  if (err) {
-    return console.log('😫', chalk.red(err));
-  }
-  console.log(`🚀 Now listening on port ${chalk.green(port)}`);
+// server.listen(port, err => {
+//   if (err) {
+//     return console.log('😫', chalk.red(err));
+//   }
+//   console.log(`🚀 Now listening on port ${chalk.green(port)}`);
+// });
+
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has Started...");
 });
