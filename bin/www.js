3c8443c9a37b5fb6ce2d38a74d04d07ae97c58e6
devilla <dev.koold@gmail.com>
2018-09-03 10:56:37 +0530

    yarn - npm fix

--- bin/www.js
+++ bin/www.js
@@ -4,20 +4,20 @@ const chalk = require('chalk');
 const http = require('http');
 const app = require('../server');
 
-// const port = '3000';
-// app.set('port', port);
+const port = process.env.PORT || '3000';
+app.set('port', port);
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// server.listen(port, err => {
-//   if (err) {
-//     return console.log('😫', chalk.red(err));
-//   }
-//   console.log(`🚀 Now listening on port ${chalk.green(port)}`);
-// });
-
-
-
-app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("Server has Started...");
+server.listen(port, err => {
+  if (err) {
+    return console.log('😫', chalk.red(err));
+  }
+  console.log(`🚀 Now listening on port ${chalk.green(port)}`);
 });
+
+
+
+// app.listen(process.env.PORT,process.env.IP,function(){
+//     console.log("Server has Started...");
+// });
